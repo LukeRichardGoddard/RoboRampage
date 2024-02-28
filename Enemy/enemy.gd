c7b5a0ad1176bb2b04cb625288b19c6c87099359
Luke Richard Goddard <github@lukerichardgoddard.com>
2024-02-28 22:05:04 +1100

    add enemy movement

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -7,27 +7,31 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+
 var player
+var provoked := false
+var aggro_range := 12.0
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 
+func _process(_delta: float) -> void:
+	if provoked:
+		navigation_agent_3d.target_position = player.global_position
+
 func _physics_process(delta: float) -> void:
-	navigation_agent_3d.target_position = player.global_position
 	var next_position = navigation_agent_3d.get_next_path_position()
+	var distance = global_position.distance_to(player.global_position)
+	
+	if distance <= aggro_range:
+		provoked = true
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = global_position.direction_to(next_position)
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -35,4 +39,4 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-#	move_and_slide()
+	move_and_slide()
