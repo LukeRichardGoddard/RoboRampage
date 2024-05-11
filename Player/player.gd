f7e60ff2802ddebbac7318772f5225df039aaed8
Luke Richard Goddard <github@lukerichardgoddard.com>
2024-05-12 02:05:49 +1000

    add weapon zooming

--- Player/player.gd
+++ Player/player.gd
@@ -6,6 +6,7 @@ const SPEED = 5.0
 @export var jump_height := 1.0
 @export var fall_multiplier := 2.5
 @export var max_hitpoints := 100
+@export var aim_multiplier := 0.7
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -15,6 +16,11 @@ var mouse_motion := Vector2.ZERO
 @onready var damage_animation_player: AnimationPlayer = $DamageTexture/AnimationPlayer
 @onready var game_over_menu: Control = $GameOverMenu
 @onready var ammo_handler: Node = %AmmoHandler
+@onready var smooth_camera: Camera3D = %SmoothCamera
+@onready var weapon_camera: Camera3D = %WeaponCamera
+
+@onready var smooth_camera_fov := smooth_camera.fov
+@onready var weapon_camera_fov := weapon_camera.fov
 
 var hitpoints: int = max_hitpoints:
 	set(value):
@@ -28,6 +34,14 @@ var hitpoints: int = max_hitpoints:
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("aim"):
+		smooth_camera.fov = lerp(smooth_camera.fov, smooth_camera_fov * aim_multiplier, delta * 20.0)
+		weapon_camera.fov = lerp(weapon_camera.fov, weapon_camera_fov * aim_multiplier, delta * 20.0)
+	else:
+		smooth_camera.fov = lerp(smooth_camera.fov, smooth_camera_fov, delta * 30.0)
+		weapon_camera.fov = lerp(weapon_camera.fov, weapon_camera_fov, delta * 30.0)
 
 func _physics_process(delta: float) -> void:
 	handle_camera_rotation()
@@ -50,6 +64,9 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		if Input.is_action_pressed("aim"):
+			velocity.x *= aim_multiplier
+			velocity.z *= aim_multiplier
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
@@ -60,6 +77,8 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			mouse_motion = -event.relative * 0.001
+			if Input.is_action_pressed("aim"):
+				mouse_motion *= aim_multiplier
 	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
